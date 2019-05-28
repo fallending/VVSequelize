902ea823d46fc08334ba278d53c046c91a460f25
Li Jinbo <pozi119@163.com>
2019-05-28 09:52:04 +0800

    Jieba preloading

--- VVSequelize/Classes/Tokenizer/Jieba/VVJieba.h
+++ VVSequelize/Classes/Tokenizer/Jieba/VVJieba.h
@@ -10,6 +10,17 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface VVJieba : NSObject
+/**
+ 预加载结巴分词资源
+ */
++ (void)preloading;
+
+/**
+ 分词
+
+ @param string 要分词的字符串
+ @param block 处理分词结果
+ */
 + (void)enumerateTokens:(const char *)string usingBlock:(void (^)(const char *token, uint32_t offset, uint32_t len, BOOL *stop))block;
 @end
 
--- VVSequelize/Classes/Tokenizer/Jieba/VVJieba.mm
+++ VVSequelize/Classes/Tokenizer/Jieba/VVJieba.mm
@@ -14,34 +14,40 @@ using namespace cppjieba;
 
 @implementation VVJieba
 
-+ (cppjieba::Jieba *)tokenizer{
++ (cppjieba::Jieba *)tokenizer {
     static cppjieba::Jieba *_tokenizer;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSBundle *currentBundle   = [NSBundle bundleForClass:self];
+        NSBundle *currentBundle = [NSBundle bundleForClass:self];
         NSString *jiebaBundlePath = [currentBundle pathForResource:@"VVJieba" ofType:@"bundle"];
-        NSBundle *jiebaBundle     = [NSBundle bundleWithPath:jiebaBundlePath];
+        NSBundle *jiebaBundle = [NSBundle bundleWithPath:jiebaBundlePath];
         const char *dictPath = [jiebaBundle pathForResource:@"jieba.dict" ofType:@"utf8"].UTF8String;
-        const char *hmmPath  = [jiebaBundle pathForResource:@"hmm_model" ofType:@"utf8"].UTF8String;
+        const char *hmmPath = [jiebaBundle pathForResource:@"hmm_model" ofType:@"utf8"].UTF8String;
         const char *userPath = [jiebaBundle pathForResource:@"user.dict" ofType:@"utf8"].UTF8String;
-        const char *idfPath  = [jiebaBundle pathForResource:@"idf" ofType:@"utf8"].UTF8String;
+        const char *idfPath = [jiebaBundle pathForResource:@"idf" ofType:@"utf8"].UTF8String;
         const char *stopPath = [jiebaBundle pathForResource:@"stop_words" ofType:@"utf8"].UTF8String;
-        
+
         _tokenizer = new Jieba(dictPath, hmmPath, userPath, idfPath, stopPath);
     });
     return _tokenizer;
 }
 
-+ (void)enumerateTokens:(const char *)string usingBlock:(void (^)(const char *token, uint32_t offset, uint32_t len, BOOL *stop))block{
++ (void)enumerateTokens:(const char *)string usingBlock:(void (^)(const char *token, uint32_t offset, uint32_t len, BOOL *stop))block
+{
     vector<Word> words;
     [VVJieba tokenizer]->CutForSearch(string, words);
     unsigned long count = words.size();
     BOOL stop = NO;
-    for (unsigned long i = 0; i < count; i ++) {
+    for (unsigned long i = 0; i < count; i++) {
         Word word = words[i];
         block(word.word.c_str(), word.offset, (uint32_t)word.word.size(), &stop);
-        if(stop) break;
+        if (stop) break;
     }
+}
+
++ (void)preloading
+{
+    [VVJieba enumerateTokens:"中文" usingBlock:^(const char *token, uint32_t offset, uint32_t len, BOOL *stop) {}];
 }
 
 @end
